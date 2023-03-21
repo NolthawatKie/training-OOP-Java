c919650ca9283fbc91dd1b65c996c3f6fbfc5636
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-21 11:10:05 +0700

    commit

--- class_and_object/Employee.java
+++ class_and_object/Employee.java
@@ -1,5 +1,33 @@
 class Employee {
-    public String id;
+    private String id;
     private String name;
     private Double salary;
+
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    // public String getId() {
+    //     return this.id;
+    // }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getSalary() {
+    return this.salary;
+    }
+
+    public void displayEmployee() {
+        System.out.println(this.id);
+        System.out.println(this.name);
+        System.out.println(this.salary);
+    }
 }
--- class_and_object/Start.java
+++ class_and_object/Start.java
@@ -1,7 +1,12 @@
 public class Start {
     public static void main(String[] args){
         Employee e1 = new Employee(); //create object
-        e1.id = "1";
-        System.out.println(e1.id);
+        e1.setId("1");
+        e1.setName("Test");
+        e1.setSalary(30000.0);
+        e1.displayEmployee();
+
+        System.out.println(e1.getName());
+
     }
 }
