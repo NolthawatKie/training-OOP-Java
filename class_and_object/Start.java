d9ea794c477aa6ad7be171bcd1e51bbf6e130377
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-21 10:39:56 +0700

    initial commit

---
+++ class_and_object/Start.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args){
+        Employee e1 = new Employee(); //create object
+        e1.id = "1";
+        System.out.println(e1.id);
+    }
+}
